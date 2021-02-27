fdaf47174ef53c64bd2f895dacf901006d9b284d
alexsominskiy <sominskiy@gmail.com>
2021-02-27 16:48:48 +0300

    nineth commit

--- 2021-02-25 Random/src/config/CarConfig.java
+++ 2021-02-25 Random/src/config/CarConfig.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class CarConfig {
 	
-public static final String[] MODELS = {"Ford", "Toyota", "Audi", "Fiat", "Bentley"};
+	public static final String[] MODELS = {"Ford", "Toyota", "Audi", "Fiat", "Bentley"};
 	
 	public static final LocalDate MIN_PRODUCTION_DATE = LocalDate.of(2000, 1, 1);   // 2000 Jan 01
 	public static final LocalDate MAX_PRODUCTION_DATE = LocalDate.of(2021, 1, 1);   // 2021 Jan 01
