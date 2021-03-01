b1fa318e52f8515da561216207b831cc72ae07dc
alexsominskiy <sominskiy@gmail.com>
2021-03-01 13:50:25 +0300

    creators and processors

--- 2021-03-01 MessageBox/src/application/MessageBoxApp.java
+++ 2021-03-01 MessageBox/src/application/MessageBoxApp.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+import implementations.CreatorsAndProcessors;
 import implementations.LinkedListMessageBox;
 import implementations.Message;
 import implementations.MessagesArrayList;
@@ -21,30 +22,6 @@ public class MessageBoxApp {
 	private static final long RECIEVER_DELAY = 200;
 	private static final int NUM_MESSAGES = 100;
 	private static final long SENDER_DELAY = 10;
-	
-	private static BiFunction<Message, Reciever<Message>, Message> messageProcessor(){
-		return (message, reciever) -> {
-			LocalDateTime recieved = LocalDateTime.now();
-			message.setRecieverName(reciever.getRecieverName());
-			message.setRecieved(recieved);
-			message.setDelay(Duration.between(message.getSent(), recieved).toMillis() + RECIEVER_DELAY);
-			
-			try {
-				Thread.sleep(RECIEVER_DELAY);									// processing imitation
-			} catch (InterruptedException e) {/* nop */}
-			
-			return message;
-		};
-	}
-	
-	private static Function<Sender<Message>, Message> messageCreator(){
-		return sender -> {
-			Message message = new Message();
-			message.setSenderName(sender.getSenderName());
-			message.setSent(LocalDateTime.now());
-			return message;
-		};
-	}
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -53,19 +30,23 @@ public class MessageBoxApp {
 		
 		Reciever<Message>[] recievers = new Reciever[NUM_RECIEVERS];
 		for (int i = 0; i < recievers.length; i++) {
-			recievers[i] = new Reciever<Message>("Reciever #"+i, messageBox, messageProcessor(), messagesCollection);
+			recievers[i] = new Reciever<Message>("Reciever #"+i, messageBox, 
+					                             CreatorsAndProcessors.messageProcessor(RECIEVER_DELAY), 
+					                             messagesCollection);
 		}
 		for (int i = 0; i < recievers.length; i++) recievers[i].start();
 		
 		Sender<Message>[] senders = new Sender[NUM_MESSAGES];
 		for (int i=0; i < senders.length; i++) {
-			senders[i] = new Sender<Message>("Sender #"+i, messageBox, messageCreator());
+			senders[i] = new Sender<Message>("Sender #"+i, messageBox, CreatorsAndProcessors.messageCreator());
 			senders[i].start();
 			Thread.sleep(SENDER_DELAY);	
 		}
 		
 		for (int i=0; i < senders.length; i++)senders[i].join();		
 		Thread.sleep(2*MESSAGE_BOX_SIZE*RECIEVER_DELAY + 1000);
+		
+		// application stop
 		
 		System.out.println("messages processed: " + messagesCollection.get().size());
 		messagesCollection.get().stream().forEach(System.out::println);
