6d52ee66fb72e377b524ff2c7c9af784406f131b
alexsominskiy <sominskiy@gmail.com>
2021-03-20 13:09:49 +0300

    first commit

---
+++ 2021-03-20 IterableAndIterator/src/application/IterableAndIteratorApp.java
@@ -0,0 +1,9 @@
+package application;
+
+public class IterableAndIteratorApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ 2021-03-20 IterableAndIterator/src/model/IterableArray.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.Iterator;
+
+public class IterableArray implements Iterable<Integer>{
+	
+	private int[] arr;
+
+	public IterableArray(int[] arr) {
+		super();
+		this.arr = arr;
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
