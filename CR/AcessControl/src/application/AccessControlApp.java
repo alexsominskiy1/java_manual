cbfa6a1d26bf48c71c6ac4605c8e10b9545fbe57
alexsominskiy <sominskiy@gmail.com>
2021-06-17 10:54:54 +0300

    mutate string removed

---
+++ CR/AcessControl/src/application/AccessControlApp.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import dto.Foo;
+
+public class AccessControlApp {
+
+	public static void main(String[] args) throws Exception {
+		
+		Foo foo = new Foo();
+		Class<Foo> fooClass = Foo.class;
+		
+		// public
+		
+		Field publicField = fooClass.getDeclaredField("publicField");
+		System.out.println(publicField.getInt(foo));
+		
+		Method publicMethod = fooClass.getDeclaredMethod("publicMethod");
+		publicMethod.invoke(foo);
+			
+		// private
+		
+		Field privateField = fooClass.getDeclaredField("privateField");
+		privateField.setAccessible(true);										
+		privateField.setInt(foo, 7);
+		System.out.println(foo);
+		
+		Method privateMethod = fooClass.getDeclaredMethod("privateMethod");
+		privateMethod.setAccessible(true);
+		privateMethod.invoke(foo);
+	}
+
+}
